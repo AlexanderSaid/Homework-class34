18da5bd8cabf2987a425a539b7bce4ae388ac1c7
AlexanderSaid <alexander.t.said@gmail.com>
2021-11-29 22:47:07 +0100

    ex6 solved

--- 3-UsingAPIs/Week2/homework/ex6-browserDebug/index.js
+++ 3-UsingAPIs/Week2/homework/ex6-browserDebug/index.js
@@ -32,8 +32,18 @@ function renderLaureate(ul, { knownName, birth, death }) {
   const li = createAndAppend('li', ul);
   const table = createAndAppend('table', li);
   addTableRow(table, 'Name', knownName.en);
-  addTableRow(table, 'Birth', `${birth.date}, ${birth.place.locationString}`);
-  addTableRow(table, 'Death', `${death.date}, ${death.place.locationString}`);
+  addTableRow(
+    table,
+    'Birth',
+    `${birth.date}, ${birth.place.locationString.en}`
+  );
+  if (death) {
+    addTableRow(
+      table,
+      'Death',
+      `${death.date}, ${death.place.locationString.en}`
+    );
+  }
 }
 
 function renderLaureates(laureates) {
@@ -43,9 +53,10 @@ function renderLaureates(laureates) {
 
 async function fetchAndRender() {
   try {
-    const laureates = getData(
+    const { laureates } = await getData(
       'http://api.nobelprize.org/2.0/laureates?birthCountry=Netherlands&format=json&csvLang=en'
     );
+    //? Isn't this a bad practice in this API to name main and sub object the same?
     renderLaureates(laureates);
   } catch (err) {
     console.error(`Something went wrong: ${err.message}`);
